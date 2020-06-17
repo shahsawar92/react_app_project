270b6f53f39959be53fa42525df7a783c444376a
shahsawar92 <shahsawar92@users.github.com>
2020-06-17 20:35:15 +0500

    hello by shahsawar

--- src/hello.js
+++ src/hello.js
@@ -2,9 +2,9 @@ import React from 'react';
 // import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return <button>hello world</button>
+function hello() {
+  return <button>my name is Muhammad Shah Sawar!</button>
 }
 
 
-export default App;
+export default hello;
