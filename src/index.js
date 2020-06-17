aad14f5616b6b80075b0f8690d328dace7a49fe9
shahsawar92 <shahsawar92@users.github.com>
2020-06-17 20:37:04 +0500

    vgjhgh

--- src/index.js
+++ src/index.js
@@ -3,9 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import hello from './hello';
 
 ReactDOM.render(
-  <App />,
+  <hello />,
   document.getElementById('root')
 );
 
